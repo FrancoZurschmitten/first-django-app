17704ad9752bae72a64c6357626a3ce60e67d0d6
franco <FrancoZurschmitten>
2023-12-08 12:09:55 -0300

    customizing admin site

--- polls/admin.py
+++ polls/admin.py
@@ -2,4 +2,12 @@ from django.contrib import admin
 
 from .models import Question, Choice
 
-admin.site.register([Question, Choice])
+
+class QuestionAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None, {"fields": ["question_text"]}),
+        ("Date information", {"fields": ["pub_date"]}),
+    ]
+
+
+admin.site.register(Question, QuestionAdmin)
