e01d31fd6e973e7bf9a97a1a263a95584c7c54f4
franco <FrancoZurschmitten>
2023-12-08 11:08:01 -0300

    creating detail.html and get obj with shortcut 'get_object_or_404'

--- polls/views.py
+++ polls/views.py
@@ -1,7 +1,7 @@
 from django.http import HttpResponse, Http404
 from .models import Question
 
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
 
 def index(request):
@@ -13,10 +13,7 @@ def index(request):
 
 
 def detail(request, question_id):
-    try:
-        question = Question.objects.get(pk=question_id)
-    except Question.DoesNotExist:
-        raise Http404("Question does not exist")
+    question = get_object_or_404(Question, pk=question_id)
     return render(request, 'polls/detail.html', {'question': question})
 
 
